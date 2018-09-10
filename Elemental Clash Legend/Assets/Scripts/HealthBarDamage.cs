c694df74f86da701f031c6631c33c8678a61cca9
Unknown <djyop974@hotmail.com>
2018-09-10 17:09:04 -0400

    Correction de bug

--- Elemental Clash Legend/Assets/Scripts/HealthBarDamage.cs
+++ Elemental Clash Legend/Assets/Scripts/HealthBarDamage.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class HealthBarDamage : MonoBehaviour {
 
-    public SimpleHealthBar healthBar;
-    int damage = 50;
-    float health = 90;
+   public SimpleHealthBar healthBar;
+    int damage = 10;
+    float health = 100;
     float maxHealth = 100;
+
     // Use this for initialization
     void Start () {
- 
+       
     }
 	
 	// Update is called once per frame
-	void Update () {
-
-        takeDamage();
+	void Update ()
+    {    
+        TakeDamage();
     }
 
-    public void takeDamage()
+    private void Awake()
     {
-        if(Input.GetKeyDown("space"))
-           {
-            health -= damage;
-            healthBar.UpdateBar(health, maxHealth);
-            print("space key was pressed");
-        }
+        healthBar = GetComponent<SimpleHealthBar>();
+    }
+
+    public void TakeDamage()
+    {
      
+        if (Input.GetKeyDown("space"))
+        {
+            health -= damage;
+            healthBar.UpdateBar(health, maxHealth);      
+            print("space key was pressed");
+        }   
     }
 }
