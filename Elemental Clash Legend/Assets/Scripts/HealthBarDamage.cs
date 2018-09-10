960e8362200badd1bca6e38520b0c70b372ef6b6
Unknown <djyop974@hotmail.com>
2018-09-10 16:30:39 -0400

    Creation du code pour update la bar

---
+++ Elemental Clash Legend/Assets/Scripts/HealthBarDamage.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarDamage : MonoBehaviour {
+
+    public SimpleHealthBar healthBar;
+    int damage = 50;
+    float health = 90;
+    float maxHealth = 100;
+    // Use this for initialization
+    void Start () {
+ 
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        takeDamage();
+    }
+
+    public void takeDamage()
+    {
+        if(Input.GetKeyDown("space"))
+           {
+            health -= damage;
+            healthBar.UpdateBar(health, maxHealth);
+            print("space key was pressed");
+        }
+     
+    }
+}
