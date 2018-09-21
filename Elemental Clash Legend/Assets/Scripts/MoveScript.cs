dda67dfe8ebdc88b2477fc9eccf27791f353fcd1
B533P-06\533 <dsgfh>
2018-09-21 16:17:34 -0400

    modification1

---
+++ Elemental Clash Legend/Assets/Scripts/MoveScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour {
+
+    // Use this for initialization
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(-1, 0);
+    //[SerializeField] GameObject Bullet;
+    [SerializeField] float fireRate = 1;
+    private Vector2 movement;
+    private float timerLastShot = 0;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var fire1 = Input.GetAxis("Jump");
+        timerLastShot += Time.deltaTime;
+        if (timerLastShot > fireRate)
+        {
+            if (fire1 > 0)
+            {
+                movement = new Vector2(speed.x, speed.y);
+                //GameObject newBullet = (GameObject)Instantiate(Bullet) as GameObject;
+                //Instantiate(Bullet, bulletEmitter.transform.position = new Vector2(5,5), bulletEmitter.transform.rotation);
+                //newBullet.transform.position = new Vector2(10, 10);
+                timerLastShot = 0;
+            }
+        }
+        
+	}
+
+}
---
+++ Elemental Clash Legend/Assets/Scripts/ShotScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour {
+
+    // Use this for initialization
+    public int damage = 1;
+    public bool isEnemyShot = false;
+    [SerializeField] GameObject Bullet;
+    void Start () {
+        Destroy(gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GameObject newBullet = (GameObject)Instantiate(Bullet) as GameObject;
+        //Instantiate(Bullet, bulletEmitter.transform.position = new Vector2(5,5), bulletEmitter.transform.rotation);
+        newBullet.transform.position = new Vector2(10, 10);
+    }
+}
