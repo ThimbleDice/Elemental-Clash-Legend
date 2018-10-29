d415086a3e77be9145e104f91ac718a16f7c3b08
Unknown <djyop974@hotmail.com>
2018-10-29 16:29:48 -0400

    Ajout UI pour potion

---
+++ Elemental Clash Legend/Assets/Scripts/CountPotion.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountPotion : MonoBehaviour {
+
+    public Text potionHealth;
+    public Text potionPower;
+    int numberPotionHealth = 0;
+    int numberPotionPower = 0;
+
+    // Use this for initialization
+    void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+        potionHealth.text = "Potion de vie : " + numberPotionHealth.ToString();
+        potionPower.text = "Potion de pouvoir : " + numberPotionPower.ToString();
+    }
+
+    public void CountPotionHealthWhenTouch()
+    {
+        numberPotionHealth += 1;        
+    }
+
+    public void CountPotionPowerWhenTouch()
+    {
+        numberPotionPower += 1;    
+    }
+}
