4d3133b69c67456472adeb69f135429a59cc7033
B533P-20\533 <pluc113@hotmail.com>
2018-11-09 16:24:05 -0500

    Ajout son pour jeu.

---
+++ Elemental Clash Legend/Assets/Scripts/ScriptPersonnage/AudioForCharacter.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioForCharacter : MonoBehaviour {
+
+    public AudioClip jumpSound;
+    public AudioClip takingDamage;
+    private AudioSource audiosource;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+        audiosource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            JumpSound();
+        }
+    }
+
+    public void JumpSound()
+    {
+        audiosource.PlayOneShot(jumpSound);
+    }
+
+    public void TakingDamageSound()
+    {
+        audiosource.PlayOneShot(takingDamage);
+    }
+}
