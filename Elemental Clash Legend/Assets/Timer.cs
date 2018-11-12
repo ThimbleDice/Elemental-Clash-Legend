3ebabf0d5f6bf1d321a5989348b3a662b7035bbf
Unknown <djyop974@hotmail.com>
2018-11-12 17:17:55 -0500

    Completer HUD

---
+++ Elemental Clash Legend/Assets/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerText;
+    private float startTime = 5.0f;
+    private float endTime = 0.0f;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        float currentTime = startTime - Time.time;
+
+        TimerFinished(currentTime);
+
+        string minutes = ((int)currentTime / 60).ToString();
+        string secondes = (currentTime % 60).ToString("f0");
+
+        timerText.text = minutes + ":" + secondes;
+    }
+
+    private void TimerFinished(float currentTime)
+    {
+        if (currentTime == endTime)
+        {
+            Debug.Log("OK");
+            timerText.color = Color.red;
+            return;
+        }
+    }
+}
