0b07ceb4c29b4dd3f649841bb803a65867f50f95
B533P-08\533 <pluc113@hotmail.com>
2018-11-12 17:35:49 -0500

    cours du 12 novembref

--- Elemental Clash Legend/Assets/Scripts/ScriptPersonnage/AudioForCharacter.cs
+++ Elemental Clash Legend/Assets/Scripts/ScriptPersonnage/AudioForCharacter.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class AudioForCharacter : MonoBehaviour {
 
+    public List<AudioClip> audioClipsListJump = new List<AudioClip>();
+
     public AudioClip jumpSound;
     public AudioClip takingDamage;
     private AudioSource audiosource;
 
+    //https://docs.unity3d.com/ScriptReference/Resources.Load.html
+    //https://answers.unity.com/questions/1186480/how-do-i-create-an-array-for-sounds.html
 
 
-
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         audiosource = GetComponent<AudioSource>();
-	}
+
+        audioClipsListJump.Add((AudioClip)Resources.Load<AudioClip>("Jumps/J1"));
+        audioClipsListJump.Add((AudioClip)Resources.Load<AudioClip>("Jumps/J2"));
+        audioClipsListJump.Add((AudioClip)Resources.Load<AudioClip>("Jumps/J3"));
+        audioClipsListJump.Add((AudioClip)Resources.Load<AudioClip>("Jumps/J4"));
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -28,7 +37,7 @@ public class AudioForCharacter : MonoBehaviour {
 
     public void JumpSound()
     {
-        audiosource.PlayOneShot(jumpSound);
+        audiosource.PlayOneShot(audioClipsListJump[Random.Range(0, audioClipsListJump.Count)]);
     }
 
     public void TakingDamageSound()
